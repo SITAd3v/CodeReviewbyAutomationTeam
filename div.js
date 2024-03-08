1b99c1adaf2ee04d221d375a3ca6f989214653c6
Nidhi Singh <nidhi.into.pgt@gmail.com>
2024-03-08 11:52:50 +0100

    reviewed

--- div.js
+++ div.js
@@ -1,3 +1,3 @@
 var x=10;
 var y=2;
-console.log(x/y)
+console.log(x/y) // ; is missing
--- logicaloperators.js
+++ logicaloperators.js
@@ -2,6 +2,7 @@ var x= 100;
 var y= 10;
 console.log(x-y)
 console.log(x+y);
-console.log(x/y)
+console.log(x/y) 
 console.log(x%y)
 console.log(x*y)
+// ; are missing
